d25b6bb032f5124ac3167d2c1ba9fc88522b9550
Bnguy777 <156261838+Bnguy777@users.noreply.github.com>
2024-05-26 23:01:22 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import About from './components/About';
 import Education from './components/Education';
@@ -10,26 +9,55 @@ import Hero from './components/Hero';
 function App() {
   return (
     <div className="App">
-      <nav className='navbar'>
-          <a href="#" class = "tab">Hero</a>
-          <a href="#" class = "tab">About</a>
-          <a href="#" class = "tab">Education</a>
-          <a href="#" class = "tab">Experience</a>
-          <a href="#" class = "tab">Extracirricular</a>
-          <a href="#Footer" class = "tab">Footer</a>
-      </nav>
-      <Hero/>
-      <About/>
-      <Education/>
-      <Experience/>
-      <Extracirricular/>
-      <section id="Footer">
-        <Footer/>
-      </section>
-      
+      <header>
+        <nav className='navbar'>
+          <a href="#Hero" class="tab">Hero</a>
+          <a href="#About" class="tab">About</a>
+          <a href="#Education" class="tab">Education</a>
+          <a href="#Experience" class="tab">Experience</a>
+          <a href="#Extracirricular" class="tab">Extracirricular</a>
+          <a href="#Footer" class="tab">Footer</a>
+        </nav>
+      </header>
+      <div className="main">
+        <div class="image-layer1"></div>  <div class="image-layer2"> <div class="image-layer3"></div></div>
+        <section id="Hero">
+          <Hero />
+        </section>
 
+        <section id="About">
+          <About />
+        </section>
+
+        <section id="Education">
+          <Education />
+        </section>
+
+        <section id="Experience">
+          <Experience />
+        </section>
+
+        <section id="Extracirricular">
+          <Extracirricular />
+        </section>
+
+        <section id="Footer">
+          <Footer />
+        </section>
+      </div>
     </div>
   );
 }
+
+const parallaxImage1 = document.querySelector('.image-layer1');
+const parallaxImage2 = document.querySelector('.image-layer2');
+
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  parallaxImage1.style.transform = `translateY(${scrollY / 5}px)`;  // Adjust division factor for slower movement
+  parallaxImage2.style.transform = `translateY(${scrollY / 3}px)`;  // Adjust division factor for medium speed
+
+});
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <h2>About</h2>
         <h4>Interests:</h4>I enjoy building things and making things run better.
         <h4>Career Goals:</h4>I plan on working a type of job that works with healthcare and programming and becoming an expert at it
-        <h4><a href="https://drive.google.com/file/d/1d9FgF2kO7NUySQToRs0qYYIQuVOCAPCV/view?usp=sharing" download="BenResume.pdf">Link to Resume</a></h4>
+        <h4><a href="https://drive.google.com/file/d/1d9FgF2kO7NUySQToRs0qYYIQuVOCAPCV/view?usp=sharing" target="https://drive.google.com/file/d/1d9FgF2kO7NUySQToRs0qYYIQuVOCAPCV/view?usp=sharing" >Link to Resume</a></h4>
         <hr></hr>
     </div>
   )
